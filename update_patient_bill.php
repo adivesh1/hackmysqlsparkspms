bfd607193ff30f41f28f1e1061e5423b7a1cdc8c
adivesh1 <adivesh@cisco.com>
2016-10-13 22:07:14 +0530

    Move Back to Home Page

--- update_patient_bill.php
+++ update_patient_bill.php
@@ -46,7 +46,7 @@ require('utils.php');
 	/*Generate PDF FOR Invoice*/
 	$filename=generate_bill_pdf($rnum);	
 	
-	$Text="**Accounts Department Update**<br>Please Find receipt which YOu Need to Pay <br> For Any Clarification Regaring Bill
+	$Text="**Accounts Department Update**<br>Please Find receipt which You Need to Pay <br> For Any Clarification Regaring Bill
 	 Please Ping us<br>Get well Soon !!<br>";
 	
 	/*Send File in Spark Room*/
