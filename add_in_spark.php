f4a11baada52cdff0280b429856df6f9631cedbf
adivesh1 <adivesh@cisco.com>
2016-10-15 15:28:03 +0530

    Some More Changes

--- add_in_spark.php
+++ add_in_spark.php
@@ -1,5 +1,8 @@
 <?php
 
+require('utils.php');
+require('spark_fun.php');
+
 $rnum=$_POST['rnum'];
 $attianed_email=$_POST['attianed_email'];
 $spark_rid="";
@@ -7,8 +10,11 @@ $spark_rid="";
 $Text="**Registration Update**<br>A New Patient Attained $attianed_email has Being Add<br>Get Well Soon !!";
 
 $spark_rid=get_sparkroom_id($rnum);
+
 add_preson_in_spark_room($spark_rid,$attianed_email);
+
 send_smg_in_spark_room($spark_rid,$Text);
+
 header("location:admin_home.php");
 
 ?>
